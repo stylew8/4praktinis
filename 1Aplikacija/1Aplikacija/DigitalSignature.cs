95220edf089ebc8694ed85f176179e2ddd763b40
stylew8 <vaniasavochkin@gmail.com>
2024-05-05 18:23:49 +0300

    ddd

---
+++ 1Aplikacija/1Aplikacija/DigitalSignature.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Aplikacija
+{
+    public class DigitalSignature
+    {
+        private  RSAParameters _privateKey;
+        private  RSAParameters _publicKey;
+
+
+        public (RSAParameters, RSAParameters) AssignNewKey()
+        {
+            using (RSA rsa = RSA.Create(2048))
+            {
+                _privateKey = rsa.ExportParameters(true);
+                _publicKey = rsa.ExportParameters(false);
+            }
+
+            return (_publicKey, _privateKey);
+        }
+
+        public byte[] SignData(string data)
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportParameters(_privateKey);
+                var dataBytes = Encoding.UTF8.GetBytes(data);
+                return rsa.SignData(dataBytes, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            }
+        }
+
+        public string ExportPublicKey()
+        {
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.ImportParameters(_publicKey);
+                return rsa.ToXmlString(false);
+            }
+        }
+    }
+}
--- 1Aplikacija/1Aplikacija/Program.cs
+++ 1Aplikacija/1Aplikacija/Program.cs
@@ -1,11 +1,48 @@
-﻿namespace _1Aplikacija
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace _1Aplikacija
 {
     class Program
     {
         static async Task Main()
         {
-            var sender = new DigitalSignatureSender();
-            sender.SendData("Hello, World!");
+            while (true)
+            {
+                var sign = new DigitalSignature();
+                try
+                {
+                    var keys = sign.AssignNewKey();
+
+                    IPAddress ipAddress = IPAddress.Loopback;
+                    int port = 11111;
+                    IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, port);
+
+                    using (Socket client = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp))
+                    {
+                        client.Connect(ipEndPoint);
+
+                        Console.WriteLine("Iveskite Teksta");
+                        string message = Console.ReadLine();
+                        var signature = sign.SignData(message);
+                        var publicKeyXml = sign.ExportPublicKey();
+                        var fullMessage = $"{message}:{Convert.ToBase64String(signature)}:{publicKeyXml}";
+                        var messageBytes = Encoding.UTF8.GetBytes(fullMessage);
+
+                        client.Send(messageBytes);
+                        Console.WriteLine("Zinute, parasas, ir viesasis raktas issiusti.");
+
+                        Console.ReadKey();
+
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
         }
     }
 }
